0f1e2256c089628ae21b1c5545b19c78aaa98980
valekar <srinivas.valekar@gmail.com>
2024-07-31 22:12:02 +0530

    :zap: add airdrop util

--- tests/utils.ts
+++ tests/utils.ts
@@ -10,3 +10,25 @@ export const addSols = async (
     "confirmed"
   );
 };
+
+export const confirmTransaction = async (tx) => {
+  const latestBlockHash = await anchor
+    .getProvider()
+    .connection.getLatestBlockhash();
+  await anchor.getProvider().connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: tx,
+  });
+};
+
+export const airdropSol = async (
+  publicKey,
+  amount = 3 * anchor.web3.LAMPORTS_PER_SOL
+) => {
+  let tx = await anchor
+    .getProvider()
+    .connection.requestAirdrop(publicKey, amount);
+
+  await confirmTransaction(tx);
+};
